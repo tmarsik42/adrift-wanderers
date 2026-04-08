3448e6305fdf1194b1b46f9e9f084e6f479a1a57
tmarsik42 <tad.marsik@gmail.com>
2026-04-08 10:41:52 +0200

    fix: Adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,6 +11,7 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
+			runtime: 'cloudflare:edge',
 			fallback: '404.html'
 		}),
 		prerender: {
